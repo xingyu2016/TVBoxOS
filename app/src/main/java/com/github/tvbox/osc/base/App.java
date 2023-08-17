35948152ad14e9eb6e4c1e6d0eff4257bf4a41a7
xingyu2016 <tufan888@gmail.com>
2023-08-17 22:43:56 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -66,6 +66,9 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
+       if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
+           Hawk.put(HawkConfig.IJK_CODEC, "硬解码");
+       } 
     }
 
     public static App getInstance() {
